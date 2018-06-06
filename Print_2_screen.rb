adca221df8bbc4de07c05bf5cde42cbb114768d0
unknown <jrhanshaw@rocketmail.com>
2018-06-06 14:24:37 -0400

    15s mined minds

--- Print_2_screen.rb
+++ Print_2_screen.rb
@@ -13,10 +13,12 @@
 
  	number = 1
  	100.times do
- 		if number % 3 == 0
+ 		if number % 15 == 0
  			puts "mined"
- 		elsif number % 5 == 0
+ 		elsif number % 3 == 0
  			puts "minds"
+ 		elsif number % 5 == 0
+ 			puts "mined minds"
  		else
  			puts number 				
  		end
