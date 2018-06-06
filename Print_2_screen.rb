8899f0b5febe1bf97aaf8b003cbb0d8d6c85b4b3
unknown <jrhanshaw@rocketmail.com>
2018-06-06 14:06:21 -0400

    replace 3s with mined, replaced 5s with minds

--- Print_2_screen.rb
+++ Print_2_screen.rb
@@ -13,10 +13,13 @@
 
  	number = 1
  	100.times do
- 		if number == 3
- 			puts "mind"
+ 		if number % 3 == 0
+ 			puts "mined"
+ 		elsif number % 5 == 0
+ 			puts "minds"
+ 		else
+ 			puts number 				
  		end
- 		puts number 
  		number = number + 1 
  	end
 
