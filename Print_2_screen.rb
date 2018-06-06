e623148f809218511b20e234cea417b69bc9bf99
unknown <jrhanshaw@rocketmail.com>
2018-06-06 14:58:05 -0400

    print_2_screen notes

--- Print_2_screen.rb
+++ Print_2_screen.rb
@@ -11,13 +11,13 @@
  # 		number = number + 1 
  # 	end
 
- 	number = 1
- 	100.times do
- 		if number % 15 == 0
+ 	number = 1 #variable for number 1
+ 	100.times do # how many times you want the vairable to run
+ 		if number % 15 == 0 #every 15th line "mined"
  			puts "mined"
- 		elsif number % 3 == 0
+ 		elsif number % 3 == 0 #every 3rd line "minds"
  			puts "minds"
- 		elsif number % 5 == 0
+ 		elsif number % 5 == 0 #every 5th line "mined minds"
  			puts "mined minds"
  		else
  			puts number 				
@@ -25,3 +25,4 @@
  		number = number + 1 
  	end
 
+README.md
