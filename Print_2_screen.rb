dab87632b9d525fee769c3be7c44f825cb402d41
unknown <jrhanshaw@rocketmail.com>
2018-06-06 12:33:22 -0400

    print to screen 1-100, 3 replaced with mind

---
+++ Print_2_screen.rb
@@ -0,0 +1,22 @@
+# p 1 
+# puts 1
+# print 1
+# 100.times do
+# 	puts 1	
+# end
+ 
+ # number = 1
+ # 	100.times do
+ # 		p number
+ # 		number = number + 1 
+ # 	end
+
+ 	number = 1
+ 	100.times do
+ 		if number == 3
+ 			puts "mind"
+ 		end
+ 		puts number 
+ 		number = number + 1 
+ 	end
+
